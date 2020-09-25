fcf5a79e4bd0d5dee0c861dc18239e435b6dd253
Leon <leon-dev@ukr.net>
2020-09-25 18:09:28 +0300

    commit

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+export class Todo{
+  title: string;
+  done: boolean;
+}
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  @Input() public todo: Todo;
+  @Output() onDone: EventEmitter<any> = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
