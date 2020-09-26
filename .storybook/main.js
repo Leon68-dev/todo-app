443abef11e6579baa31837529fa5253c0176e007
Leon <leon-dev@ukr.net>
2020-09-26 18:52:37 +0300

    commit

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,11 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)",
-    "../src/*.stories\.ts$"
+    //"../src/**/*.stories.mdx",
+    //"../src/**/*.stories.@(js|jsx|ts|tsx)",
+    '../src/app/todo/**/*.stories.ts'
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
   ]
 }
-
-// configure(require.context('../src', true, /\.stories\.ts$/), module);
